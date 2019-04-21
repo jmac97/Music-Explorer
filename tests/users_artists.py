0a12d729e0da3a421a0e03289c0d49f70f4e2fef
JSM <47127982+jmac97@users.noreply.github.com>
2019-04-20 22:41:20 -0400

    Update users_artists.py

--- tests/users_artists.py
+++ tests/users_artists.py
@@ -1,11 +1,15 @@
+'''
+Tests if code can user's top five most listened to artists (recent)
+Tests if code can generate list of recommended songs based on the above
+Enter user ID for username below
+'''
+
 import spotipy
 import spotipy.util as util
 import json
 from json import JSONDecodeError
 
-# User ID: 1224649854
-
-username = '1224649854'
+username = 'X'
 
 token = util.prompt_for_user_token(username, scope='playlist-modify-private,playlist-modify-public,user-top-read', client_id='39072f510ea74d4781f2423c61bb4c27',client_secret='82a83ddeb06743e0956bcbb296856412',redirect_uri='https://www.google.com/')
 
@@ -23,14 +27,9 @@ print("List of your top artists:")
 for item in results['items']:
     topArtists.append(item['id'])
     print(item['name'])
-
-#print(artistID)
-
-
-
+print()
 
 #print(json.dumps(results,sort_keys=True,indent=4))
-
 
 recs = spotifyObject.recommendations(seed_artists=[topArtists[0],topArtists[1],topArtists[2],topArtists[3],topArtists[4]])
 print("List of recommended songs based on your request:")
