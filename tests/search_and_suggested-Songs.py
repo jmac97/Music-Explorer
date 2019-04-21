6340eb0d6bbf823cf5ed382cc408d2701405df50
JSM <47127982+jmac97@users.noreply.github.com>
2019-04-20 22:32:23 -0400

    Update and rename song.py to search_and_suggested-Songs.py

--- tests/search_and_suggested-Songs.py
+++ tests/search_and_suggested-Songs.py
@@ -1,11 +1,15 @@
+'''
+Tests if a searched song can be fetched
+Tests if a list of recommended songs based off of search item can be created and printed
+Enter your username idea in the below code
+'''
+
 import spotipy
 import spotipy.util as util
 import json
 from json import JSONDecodeError
 
-# User ID: 1224649854
-
-username = '1224649854'
+username = 'X'
 
 token = util.prompt_for_user_token(username, scope='playlist-modify-private,playlist-modify-public', client_id='39072f510ea74d4781f2423c61bb4c27',client_secret='82a83ddeb06743e0956bcbb296856412',redirect_uri='https://www.google.com/')
 
