86edf9d39278eed36b5b0ff15a1b76dae0dad2b6
JSM <47127982+jmac97@users.noreply.github.com>
2019-04-20 22:29:25 -0400

    Update and rename listgenres.py to list_genres.py

--- tests/list_genres.py
+++ tests/list_genres.py
@@ -1,11 +1,12 @@
+'''
+Prints out list of available genre seeds that the API can use
+Tests if genre data can be acquired
+'''
+
 import spotipy
 import spotipy.util as util
 import json
 from json import JSONDecodeError
-
-
-# User ID: 1224649854
-username = '1224649854'
 
 token = util.prompt_for_user_token(username, scope='playlist-modify-private,playlist-modify-public', client_id='39072f510ea74d4781f2423c61bb4c27',client_secret='82a83ddeb06743e0956bcbb296856412',redirect_uri='https://www.google.com/')
     
@@ -14,13 +15,12 @@ spotifyObject.trace = False
 
 user = spotifyObject.current_user()
 
-
-gdict =  spotifyObject.recommendation_genre_seeds()
+getDict =  spotifyObject.recommendation_genre_seeds()
     
 #print(json.dumps(genres,sort_keys=True,indent=4))
 
 genres = list(gdict.values())
-ugh = genres[0]
+item = genres[0]
 
-for i in ugh:
+for i in item:
     print(i)
